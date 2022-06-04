9b168fc981610f6b5f55a54dff6bbed12b9dfe20
wolskey <sebexiv@gmail.com>
2022-06-04 13:50:05 +0200

    updated contract

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <s.Container jc={"center"} ai={"center"} style={{ flexDirection: "row", width: "70%" }}s>
         <s.TextFooter style={{ textAlign: "center", color: "var(--primary-text)"}} >
-        <a href="/" style={{ color: "var(--primary-text)"}}>EthLlamas, LLC</a> - <a href="/license" style={{ color: "var(--primary-text)"}}>License</a> - <a href="ETHLLAMAS_Audit_Passed.pdf" style={{ color: "var(--primary-text)"}}>Audit</a>
+        <a href="/" style={{ color: "var(--primary-text)"}}>Check your neck - this is Vampirestown.nft</a>
         </s.TextFooter>
       </s.Container>
     )
